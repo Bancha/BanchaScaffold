0bd3be63a595d6c505dfb4ee98f202990fb7b6b2
Roland Schütz <mail@rolandschuetz.at>
2012-06-14 18:31:27 +0200

    Split sample code into two files for cleaner code

--- samples/js/samples.js
+++ samples/js/samples.js
@@ -20,198 +20,18 @@
 /*jshint bitwise:true, curly:true, eqeqeq:true, forin:true, immed:true, latedef:true, newcap:true, noarg:true, noempty:true, regexp:true, undef:true, trailing:false */
 /*global Ext, Bancha */
 
-// API and Bancha is already included,
-// now load sample dependencies
-Ext.require([
-    'Ext.form.*',
-    'Ext.grid.*'
-]);
 
-// init Provider
-Ext.direct.Manager.addProvider(Bancha.REMOTE_API);
-
-
-// setup the path to the delete image in local environments
-Bancha.scaffold.Grid.destroyButtonConfig.items[0].icon = 'img/icons/delete.png';
-
-Ext.define('Bancha.model.Article', {
-    extend: 'Ext.data.Model',
-    proxy: {
-        type: 'direct',
-        batchActions: false,
-        api: {
-            read    : Bancha.RemoteStubs.Article.read,
-            create  : Bancha.RemoteStubs.Article.create,
-            update  : Bancha.RemoteStubs.Article.update,
-            destroy : Bancha.RemoteStubs.Article.destroy
-        },
-        reader: {
-            type: 'json',
-            root: 'data',
-            messageProperty: 'message'
-        },
-        writer: {
-            type: 'json',
-            writeAllFields: false,
-            root: 'data'
-        }
-    },
-    idProperty:'id',
-    fields:[
-        {
-            name:'id',
-            type:'int'
-        },{
-            name:'title',
-            type:'string'
-        },{
-            name:'date',
-            type:'date',
-            dateFormat:'Y-m-d H:i:s'
-        },{
-            name:'body',
-            type:'string'
-        },{
-            name:'published',
-            type:'boolean'
-        },{
-            name:'user_id',
-            type:'int'
-        }],
-        validations:[{
-            type:'presence',
-            field:'title'
-        },{
-            type:'numberformat',
-            field:'user_id'
-        }
-    ],
-    associations:[
-        {
-          type:'belongsTo',
-           model:'Bancha.model.User',
-           name:'users'
-        }
-    ]
-});
-Ext.define('Bancha.model.User', {
-    extend: 'Ext.data.Model',
-    proxy: {
-        type: 'direct',
-        batchActions: false,
-        api: {
-            read    : Bancha.RemoteStubs.User.read,
-            create  : Bancha.RemoteStubs.User.create,
-            update  : Bancha.RemoteStubs.User.update,
-            destroy : Bancha.RemoteStubs.User.destroy
-        },
-        reader: {
-            type: 'json',
-            root: 'data',
-            messageProperty: 'message'
-        },
-        writer: {
-            type: 'json',
-            writeAllFields: false,
-            root: 'data'
-        }
-    },
-    idProperty: 'id',
-    fields: [
-        {
-            name: 'id',
-            type: 'int'
-        }, {
-            name: 'name',
-            type: 'string'
-        }, {
-            name: 'login',
-            type: 'string'
-        }, {
-            name: 'created',
-            type: 'date',
-            dateFormat: 'Y-m-d H:i:s'
-        }, {
-            name: 'email',
-            type: 'string'
-        }, {
-            name: 'avatar',
-            type: 'string'
-        }, {
-            name: 'weight',
-            type: 'float'
-        }, {
-            name: 'height',
-            type: 'int'
-        }
-    ],
-    validations: [
-        {
-            type: 'numberformat',
-            field: 'id',
-            'precision': 0
-        }, {
-            type: 'presence',
-            field: 'name'
-        }, {
-            type: 'length',
-            field: 'name',
-            min: 3,
-            max: 64
-        }, {
-            type: 'presence',
-            field: 'login'
-        }, {
-            type: 'length',
-            field: 'login',
-            min: 3,
-            max: 64
-        }, {
-            type: 'format',
-            field: 'login',
-            matcher: /^[a-zA-Z0-9_]+$/ // alphanum regex
-        }, {
-            type: 'presence',
-            field: 'email'
-        }, {
-            type: 'format',
-            field: 'email',
-            matcher: /^(\w+)([\-+.][\w]+)*@(\w[\-\w]*\.){1,5}([A-Za-z]){2,6}$/ // email regex
-        }, {
-            type: 'numberformat',
-            field: 'weight',
-            precision: 2
-        }, {
-            type: 'numberformat',
-            field: 'height',
-            precision: 0
-        }, {
-            type: 'numberformat',
-            field: 'height',
-            min: 50,
-            max: 300
-        }, {
-            type: 'file',
-            field: 'avatar',
-            extension: ['gif', 'jpeg', 'png', 'jpg']
-        }
-    ],
-    associations: [
-        {
-            type: 'hasMany',
-            model: 'Bancha.model.Article',
-            name: 'articles'
-        }
-    ]
-});
-
+// wait for DOM to be ready
 Ext.onReady(function() {
+
+
 
     // ... create a full featured users grid
     Ext.create('Ext.grid.Panel', {
         title: 'Easily scaffold full CRUD support, based on the models proxy',
 
-        scaffold: 'Bancha.model.User', // model name
+        // define the model
+        scaffold: 'Bancha.model.User', 
         
         // basic scaffold configs con be set directly
         enableCreate : true,
@@ -226,6 +46,8 @@ Ext.onReady(function() {
         renderTo: 'gridpanel'
     });
 
+
+
     // ... a simple example how you can extend it with regular code
     Ext.create('Ext.grid.Panel', {
         title: 'Starting from the Scaffoldign you can easily add your own code',
@@ -235,9 +57,9 @@ Ext.onReady(function() {
         enableUpdate: false,
         enableDestroy: false,
         
-        // configure scaffolding
+        // more advanced scaffolding options
         scaffold: {
-            // model name
+            // define the model
             target: 'Bancha.model.Article',
 
             // configure paging
@@ -267,6 +89,8 @@ Ext.onReady(function() {
         renderTo: 'paginated-gridpanel'
     });
 
+
+
     // and a form panel
     Ext.create('Ext.form.Panel', {
         title: 'Demonstration of a scaffolded Form',
@@ -277,13 +101,13 @@ Ext.onReady(function() {
         
         // model name and advanced configs can be set here
         scaffold: {
-            // model name
+            // define the model
             target: 'Bancha.model.User',
             
             // we're using the after interceptor for more complex changes
             afterBuild: function(formConfig) {
                 
-                // add another button
+                // add another button at the left side
                 formConfig.buttons.unshift({
                     text: 'Load Sample Record',
                     iconCls: 'icon-edit-user',
